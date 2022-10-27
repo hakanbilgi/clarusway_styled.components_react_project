de5d90c3a4b03cb15d116cbf8e9280bee3c24380
Hakan Bilgi <hknblg34@gmail.com>
2022-10-27 16:11:46 +0300

    Card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./components/styles/Global.styled";
+import Card from "./components/Card";
 
 const style = {
   colors: {
@@ -17,6 +18,7 @@ const App = () => {
     <ThemeProvider theme={style}>
       <GlobalStyles/>
       <Header />
+      <Card/>
     </ThemeProvider>
   );
 };
