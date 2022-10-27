7946fe13bc5dfdcbda8b0b5b798e20716f23a43a
Hakan Bilgi <hknblg34@gmail.com>
2022-10-27 18:50:47 +0300

    icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import FooterDiv, { FooterContent, FooterLogo } from "./styles/Footer.styled";
+import FooterDiv, {
+  FooterContent,
+  FooterFlex,
+  FooterLogo,
+  SocialIcons,
+} from "./styles/Footer.styled";
+import { FaTwitterSquare, FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 
-const Footer = () => {
-  return (
-    <FooterDiv>
-      <FooterContent>
-        <div>
-          <FooterLogo src="./images/logo_white.png" />
-        </div>
-        <div>
-            
-          <ul>
-            <li>1775 Tysons Blvd. 5th Floor. Tysons, VA 22102</li>
-            <li>+1 (571) 360 66 77</li>
-            <li>contact@clarusway.com</li>
-          </ul>
-          <ul>
-            <li>About Us</li>
-            <li>What We Do</li>
-            <li>FAQ</li>
-          </ul>
-          <ul>
-            <li>Career</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
-          </ul>
 const Footer = () => {
   return (
     <FooterDiv>
@@ -49,11 +31,21 @@ const Footer = () => {
             <li>Blog</li>
             <li>Contact Us</li>
           </ul>
+          <SocialIcons>
+            <a href="http://www.twitter.com">
+              <FaTwitterSquare />
+            </a>
+            <a href="http://www.facebook.com">
+              <FaFacebookSquare />
+            </a>
+            <a href="http://www.linkedin.com">
+              <FaLinkedin />
+            </a>
+          </SocialIcons>
         </FooterFlex>
       </FooterContent>
     </FooterDiv>
   );
 };
-
 
 export default Footer;
--- src/components/styles/Footer.styled.jsx
+++ src/components/styles/Footer.styled.jsx
@@ -14,13 +14,30 @@ export const FooterContent = styled.div`
 export const FooterLogo = styled.img`
   width: 320px;
   margin: 1rem 0;
-  @media (max-width: ${({ theme }) => theme["max-width"]}) {
-    width:90%;
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    width: 90%;
     display: block;
     margin: 0 auto;
-
   }
 `;
-export const FooterFlex =styled.div`
 
+export const FooterFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ul {
+    list-style-type: none;
+  }
+  li {
+    margin-bottom: 1rem;
+    padding: 0 0.4rem;
+  }
+  @media (max-width: ${({ theme }) => theme.responsive}) {
+    flex-direction: column;
+    text-align: center;
+    ul {
+      padding: 1.8rem 0;
+    }
+  }
 `;
+
+export const SocialIcons = styled(FooterFlex)``;
