0a1351e5c1269699b2a68f273b95557dc92e8be8
Hakan Bilgi <hknblg34@gmail.com>
2022-10-27 13:12:35 +0300

    theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import Button from "./styles/Button.styled"
 
 const Header = () => {
   return (
-   <div>
+   <header>
    <Button color="#A62440">Apply Courses</Button>
    <Button bg="#A62440">Talk to Adviser</Button>
-   </div>
+   </header>
   );
 }
 
