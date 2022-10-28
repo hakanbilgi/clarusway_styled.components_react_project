be8c6698641a1b04fac78cc412d203382665c191
Hakan Bilgi <hknblg34@gmail.com>
2022-10-28 17:30:58 +0300

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import FooterDiv, { FooterContent, FooterFlex, FooterLogo, SocialIcons } from "./styles/Footer.styled";
+import FooterDiv, {
+  FooterContent,
+  FooterFlex,
+  FooterLogo,
+  SocialIcons,
+} from "./styles/Footer.styled";
 
 const Footer = () => {
   return (
