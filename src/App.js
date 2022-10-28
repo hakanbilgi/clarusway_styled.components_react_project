31a8b94e2b60714e136d1d5cf517d7e089880492
Hakan Bilgi <hknblg34@gmail.com>
2022-10-28 17:21:20 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/styles/Global.styled";
 import Card from "./components/Card";
-import Footer from "./components/Footer";
 
 const style = {
-  colors: {
+  color: {
     header: "#fff",
     body: "#eee",
     footer: "#8A1C4A",
   },
-  
   margins: {},
 
-  "max-width":"1000px",
-  width:"95vw",
-  responsive: "724px",
+  "max-width": "1000px",
+  width: "95vw",
+
+  responsive: "768px",
 };
 
 const App = () => {
   return (
     <ThemeProvider theme={style}>
-      <GlobalStyles />
       <Header />
       <Card />
-     <Footer/>
+      <Footer />
     </ThemeProvider>
   );
 };
