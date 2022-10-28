9464c32072c2f5fd45d8a543b37e07f202ddce9a
Hakan Bilgi <hknblg34@gmail.com>
2022-10-28 17:24:47 +0300

    card styled

--- src/components/styles/Card.styled.jsx
+++ src/components/styles/Card.styled.jsx
@@ -1,34 +1,28 @@
 import styled from "styled-components";
 
-const Main = styled.main`
+export const Main = styled.div`
   width: ${({ theme }) => theme.width};
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
   background-color: #fff;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   padding: 4rem;
-  margin: 1rem auto 0;
+  margin: 1rem auto;
   gap: 3rem;
   max-width: ${({ theme }) => theme["max-width"]};
   flex-direction: ${({ id }) => (id % 2 ? "row" : "row-reverse")};
 
   @media (max-width: ${({ theme }) => theme.responsive}) {
-    flex-direction: column;
+    /* flex-direction: column; */
     text-align: center;
     flex-direction: column-reverse;
   }
-`;
-export const CardTitle = styled.h2`
-  font-weight: bolder;
-  margin: 1rem 0.4rem;
 `;
 
 export const CardImg = styled.img`
   width: 90%;
   object-fit: cover;
-  /* min-width: 150px; */
-
   margin: 20px;
   border-radius: 10px;
   @media (max-width: ${({ theme }) => theme.responsive}) {
@@ -36,10 +30,18 @@ export const CardImg = styled.img`
   }
 `;
 
-export const Description = styled.p`
-  max-width: 500px;
-  min-width: 250px;
-  font-size: 1.1rem;
+export const CardTitle = styled.h2`
+font-weight: bolder;
+margin: 1rem 0.4rem;
+
+
 `;
 
-export default Main;
+export const Description = styled.p`
+max-width: 500px;
+min-width: 250px;
+font-size: 1.1rem;
+`;
+
+
+
