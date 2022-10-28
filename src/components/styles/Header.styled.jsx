1e9b7baa3622d98d2ce39463eccbc9eddc0a6c3c
Hakan Bilgi <hknblg34@gmail.com>
2022-10-28 17:27:35 +0300

    header styled

--- src/components/styles/Header.styled.jsx
+++ src/components/styles/Header.styled.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
-  background-color: ${({ theme }) => theme.colors.header};
-   max-width: ${({ theme }) => theme["max-width"]};
-   margin: auto;
+  background: ${({ theme }) => theme.color.header};
+  max-width: ${({ theme }) => theme["max-width"]};
+  margin: auto;
   padding: 4rem 0;
-  
 `;
+
+export default StyledHeader;
 
 export const Logo = styled.img`
   width: 300px;
@@ -15,18 +16,16 @@ export const Logo = styled.img`
 
 export const Nav = styled.nav`
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  @media (max-width: ${({ theme }) => theme.responsive}) {
-    flex-direction: column;
-    /* flex-wrap: wrap; */
-  }
+  align-items: center;
+  @media (max-width: ${({ theme }) => theme.responsive}){
+  flex-direction: column;
+  },
 `;
 
 export const Image = styled(Logo)`
-  @media (max-width: ${({ theme }) => theme.responsive}) {
-    margin: 2rem 0;
-  }
-`;
+@media (max-width: ${({ theme }) => theme.responsive}){
+  margin: 2rem 0;
+  
 
-export default StyledHeader;
+`;
